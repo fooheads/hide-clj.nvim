78fbf5b4ee1f915f33a1cfa98077aa822f7794bb
Niclas Nilsson <niclas@niclasnilsson.se>
2018-03-27 08:17:25 +0100

    Implement neovim RPC communication.

--- src/fooheads/hide_clj/nvim/core.clj
+++ src/fooheads/hide_clj/nvim/core.clj
@@ -1,36 +1,85 @@
 (ns fooheads.hide-clj.nvim.core
-  (:require [neovim-client.1.api :as api] 
-            [neovim-client.nvim :as nvim]))
+  (:require [clojure.core.async :as async] 
+            [clojure.tools.logging :as log]
+            [msgpack.clojure-extensions]
+            [msgpack.core :as msgpack]
+            ))
 
 ;; 
 ;; hide-clj.nvim 
 ;; 
 
+(defn connect []
+  (let [host "localhost"
+        port 7777
+        socket (java.net.Socket. host port)
+        istream (.getInputStream socket)
+        ostream (.getOutputStream socket)]
+    (.setTcpNoDelay socket true)
 
-(defn -main [& args]
-  (println "Starting Hide for Neovim.")
+    {:input-stream istream
+     :output-stream ostream}))
 
-  ; Start Neovim with: 
-  ;   NVIM_LISTEN_ADDRESS=127.0.0.1:7777 nvim
-  (let [conn (neovim-client.nvim/new 1 "localhost" 7777)
-        x (atom 0)]
+(defn start-repl []
+  (clojure.core.server/start-server 
+    {:address "localhost" :port 8888 :name "hide-neovim-repl" :accept 'clojure.core.server/repl})
+  )
 
-    ; This can be read from Neovim
-    (api/command conn ":let g:is_running=1")
+(defn event-loop [input-stream output-stream]
+  (loop []
+    (log/info "waiting for message...")
 
-    ; TODO: Shouldn't we be able to call these functions from 
-    ;       Neovim?
-    (nvim/register-method!
-      conn
-      "foo"
-      (fn [msg]
-        (println "Clojure received '" msg "'")))
+    (when-let [msg (msgpack/unpack input-stream)]
+      (log/info "->m " msg)
+      (let [[msg-type & msg-data] msg]
+        ; (log/debug "  msg-type:" msg-type)
+        (log/debug "  msg-data:" msg-data)
 
-    (dotimes [n 60]
-      (if (= 0 (mod n 10))
-        (api/command conn (str ":echo 'plugin alive for " n " seconds.'")))
-      (Thread/sleep 1000))
+        (case msg-type 
+          0 ; request
+          (let [[channel func args] msg-data]
+            (log/debug "  rpcrequest: ")
+            (log/debug "    channel: " channel)
+            (log/debug "    func: " func)
+            (log/debug "    args: " args)
 
-    ;; Let nvim know we're shutting down.
-    (api/command conn ":let g:is_running=0")
-    (api/command conn ":echo 'plugin stopping.'"))) 
+            (do 
+              ;; We really need to respond something, in order to 
+              ;; not hang neovim.
+              (let [response-msg [1 channel nil (str "hej! på! dig! " func)] 
+                    packed (msgpack/pack response-msg)]
+                (.write output-stream packed 0 (count packed))     
+                (.flush output-stream)
+                (log/info "<-m " response-msg)
+                ))
+
+            (if-not (= func ":quit") 
+              (recur)
+              (log/debug "received :quit. Quitting.")))
+
+          1 ; response
+          (let []
+            (log/debug "  rpcresponse: ")
+            (throw (Exception. "Not Yet Implemented.")))
+
+          2 ; notify
+          (let [[func args] msg-data]
+            (log/debug "type: " (class msg-data) (class (first msg-data)))
+            (log/debug "  rpcnotify: ")
+            (log/debug "    func: " func)
+            (log/debug "    args: " args)
+            (log/debug "<-m")
+
+            (if-not (= func ":quit") 
+              (recur)
+              (log/debug "received :quit. Quitting.")))
+
+          (throw (Exception. (str "Unsupported msg-type: " msg-type))))))))
+
+(defn start []
+  (let [conn (connect)
+        input-stream (:input-stream conn)
+        output-stream (:output-stream conn)]
+    (async/thread-call (partial event-loop input-stream output-stream))))
+
+
